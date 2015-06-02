32dcf2dabf631063b7ec957c50ee305eee4bed0d
Casey Allred <casey.allred@fanzz.com>
2015-06-02 11:50:47 -0600

    code 'done enough'

---
+++ awesome/src/lib.rs
@@ -0,0 +1,169 @@
+//! the awesome crate is here to help will all your wildest dreams
+//! # Example
+//! ```
+//! assert_eq!(awesome::fizz_buzz(3), "fizz");
+//! assert_eq!(awesome::fib(1), 1);
+//! ```
+use std::borrow::Cow;
+use std::fmt::{Display, Error, Formatter};
+use std::cmp::PartialEq;
+
+/// Fizz Buzz'n function
+/// # Fizz Buzz Example
+/// ```
+/// # extern crate awesome;
+/// # use awesome::fizz_buzz;
+/// # fn main () {
+///    for n in (0..10).map(fizz_buzz) {
+///       println!("{}", n);
+///    }
+/// # }
+/// ```
+pub fn fizz_buzz(n: u64) -> Cow<'static, str> {
+    match (n % 3, n % 5) {
+        (0, 0) => "fizzbuzz".into(),
+        (0, _) => "fizz".into(),
+        (_, 0) => "buzz".into(),
+        _      => n.to_string().into()
+    }
+}
+
+/// Fizz Buzz'n function done with enums
+/// # Fizz Buzz Enum Example
+/// ```
+/// # extern crate awesome;
+/// # use awesome::fizz_buzz_enum;
+/// # fn main () {
+///    for n in (0..10).map(fizz_buzz_enum) {
+///       println!("{}", n);
+///    }
+/// # }
+/// ```
+#[derive(Debug)]
+pub enum FizzBuzz {
+    FizzBuzz,
+    Buzz,
+    Fizz,
+    Number(u64),
+}
+impl Display for FizzBuzz {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match *self {
+            FizzBuzz::FizzBuzz  => write!(f, "fizzbuzz"),
+            FizzBuzz::Fizz      => write!(f, "fizz"),
+            FizzBuzz::Buzz      => write!(f, "buzz"),
+            FizzBuzz::Number(n) => write!(f, "{}", n),
+        }
+    }
+}
+impl PartialEq for FizzBuzz {
+    fn eq(&self, other: &FizzBuzz) -> bool {
+        match (self, other) {
+            (&FizzBuzz::FizzBuzz, &FizzBuzz::FizzBuzz)                 => true,
+            (&FizzBuzz::Fizz, &FizzBuzz::Fizz)                         => true,
+            (&FizzBuzz::Buzz, &FizzBuzz::Buzz)                         => true,
+            (&FizzBuzz::Number(n1), &FizzBuzz::Number(n2)) if n1 == n2 => true,
+            _                                                          => false,
+        }
+    }
+}
+pub fn fizz_buzz_enum(n: u64) -> FizzBuzz {
+    match (n % 3, n % 5) {
+        (0, 0) => FizzBuzz::FizzBuzz,
+        (0, _) => FizzBuzz::Fizz,
+        (_, 0) => FizzBuzz::Buzz,
+        _      => FizzBuzz::Number(n)
+    }
+}
+
+/// Fib'n your number iterative style
+/// # Fib Example
+/// ```
+/// for n in (1..10).map(awesome::fib) {
+///    println!("{}", n);
+/// }
+/// ```
+pub fn fib(n: u64) -> u64 {
+    if n < 2 {
+        n
+    } else {
+        let mut prev = 1;
+        let mut now = 1;
+        for _ in (1 .. n) { // already done with 2 and range is exclusive for n
+            let tmp = now;
+            now = prev;
+            prev = prev + tmp;
+        }
+        now
+    }
+}
+
+/// Fib'n your number in the new recursive awesome way
+/// # Fib Recursive Example
+/// ```
+/// for n in (1..10).map(awesome::fib_rec) {
+///    println!("{}", n);
+/// }
+/// ```
+pub fn fib_rec(n: u64) -> u64 {
+    fn f(p: u64, n: u64, i: u64) -> u64 {
+        if i == 0 {
+            n
+        } else {
+            f(n,p+n,i-1)
+        }
+    }
+    if n < 2 {
+        n
+    } else {
+        f(1,1, n - 2) //first 2 values are taken care of
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn fizz_buzz_one_through_fifteen() {
+        let pairs = [(1, "1"), (2, "2"), (3, "fizz"), (4, "4"), (5, "buzz"),
+                     (6, "fizz"), (7, "7"), (8, "8"), (9, "fizz"), (10, "buzz"),
+                     (11, "11"), (12, "fizz"), (13, "13"), (14, "14"), (15, "fizzbuzz")];
+
+        for &(i, res) in pairs.iter() {
+            assert_eq!(fizz_buzz(i), res);
+        }
+        
+    }
+    
+    #[test]
+    fn fizz_buzz_enum_test() {
+        let pairs = [(1, FizzBuzz::Number(1)), (3, FizzBuzz::Fizz), (4, FizzBuzz::Number(4)), (5, FizzBuzz::Buzz), (15, FizzBuzz::FizzBuzz)];
+
+        for &(i, ref res) in pairs.iter() {
+            assert_eq!(&fizz_buzz_enum(i), res);
+        }
+        
+    }
+
+    #[test]
+    fn fib_zero_through_nine(){
+        let pairs = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3),
+                     (5, 5), (6, 8), (7, 13), (8, 21), (9, 34)];
+
+        for &(i, res) in pairs.iter() {
+            assert_eq!(fib(i), res);
+        }
+    }
+    
+    #[test]
+    fn fib_rec_zero_through_nine(){
+        let pairs = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3),
+                     (5, 5), (6, 8), (7, 13), (8, 21), (9, 34)];
+
+        for &(i, res) in pairs.iter() {
+            assert_eq!(fib_rec(i), res);
+        }
+    }
+
+}
--- fizz_buzz/src/main.rs
+++ fizz_buzz/src/main.rs
@@ -6,16 +6,22 @@ extern crate router;
 extern crate staticfile;
 extern crate awesome;
 
+
+use std::path::Path;
+use std::thread;
+use std::sync::{Arc, mpsc, Mutex};
+
 use iron::{Iron, IronResult, Protocol, Request, Response, status};
 use router::{Router};
 use mount::Mount;
 use staticfile::Static;
-use std::path::Path;
-use awesome::{fizz_buzz, fib, fib_rec};
+
+use awesome::{fizz_buzz, fizz_buzz_enum, fib, fib_rec};
 
 fn main() {
     //For routing dynamic handlers
     let mut router = Router::new();
+    router.get("/fizzbuzz_enum/:query", fizz_buzz_enum_handler);
     router.get("/fizzbuzz/:query", fizz_buzz_handler);
     router.get("/fib/:query", fib_handler);
     router.get("/fib_rec/:query", fib_rec_handler);
@@ -31,35 +37,78 @@ fn main() {
 
     fn handle_query<F>(query: &str, f: F) -> String where F: Fn(u64) -> String {
         let default = 0;
-        let n: Vec<u64> = (*query).replace("%20","").split(",").map(|n| n.trim().parse::<u64>().unwrap_or(default)).collect();
+        let ns: Vec<u64> = (*query).replace("%20","").split(",").map(|n| n.trim().parse::<u64>().unwrap_or(default)).collect();
         let mut resp = String::from_str("Result: ");
-        for (i, nstr) in  n.iter().map(|n| f(*n)).enumerate() {
+        for (i, nstr) in  ns.iter().map(|n| f(*n)).enumerate() {
             if i != 0 {
                 resp.push_str(", ");
             }
             resp.push_str(&nstr);
         }
         resp
-    } 
+    }
 
+    //Handle query with THREADS! and CHANNELS!
+    fn handle_query_threaded<F>(query: &str, f: F) -> String where F: Fn(u64) -> String + Send + 'static {
+        let default = 0;
+        let ns: Vec<u64> = (*query).replace("%20","").split(",").map(|n| n.trim().parse::<u64>().unwrap_or(default)).collect();
+
+        let (tx, rx) = mpsc::channel();
+
+        let f_arc = Arc::new(Mutex::new(f));
+        
+        for (i, n) in  ns.iter().enumerate() {
+            let tx = tx.clone();
+            let f = f_arc.clone();
+            let i_clone = i.clone();
+            let n_clone = n.clone();
+            thread::spawn(move || {
+                let answer = f.lock().unwrap()(n_clone);
+
+                if i_clone == 0 {
+                    tx.send(answer).unwrap();
+                } else {
+                    tx.send(", ".to_string() + &answer).unwrap();
+                }
+                
+            });
+        }
+
+        let mut resp = String::from_str("Result: ");
+        for _ in ns {
+            match rx.recv() {
+                Ok(str) => resp.push_str(&str),
+                Err(_) => resp.push_str("ERROR"),
+            }
+        }
+        resp
+    }
+
+    //Send back the fizzbuzz_enum of all the things
+    fn fizz_buzz_enum_handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("0");
+        let resp = handle_query(query, |n| fizz_buzz_enum(n).to_string());
+        Ok(Response::with((status::Ok, resp)))
+    }
+    
     //Send back the fizzbuzz of all the things
     fn fizz_buzz_handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("0");
         let resp = handle_query(query, |n| fizz_buzz(n).to_string());
         Ok(Response::with((status::Ok, resp)))
     }
     
     //Send back the fib of all the things
     fn fib_handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
-        let resp = handle_query(query, |n| fib(n).to_string());
+        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("0");
+        let resp = handle_query_threaded(query, |n| fib(n).to_string());
         Ok(Response::with((status::Ok, resp)))
     }
 
     //Send back the fib of all the things
     fn fib_rec_handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
-        let resp = handle_query(query, |n| fib_rec(n).to_string());
+        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("0");
+        let resp = handle_query_threaded(query, |n| fib_rec(n).to_string());
         Ok(Response::with((status::Ok, resp)))
     }
 }
